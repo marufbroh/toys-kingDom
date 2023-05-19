3b22a221f58e2fb4cb2c6b1ace532651c6801e00
marufbroh <marufmd775@gmail.com>
2023-05-19 22:20:49 +0600

    banner section complete

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
     return (
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={mobileBanner} className="w-full" />
+                <img src={mobileBanner} className="w-full lg:hidden" />
                 <img src={largeBanner} className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
@@ -17,7 +17,7 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src={mobileBanner2} className="w-full" />
+                <img src={mobileBanner2} className="w-full lg:hidden" />
                 <img src={largeBanner2} className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
